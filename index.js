cac4049b6e0743dbab1de274facb825883604835
Melsom <henrik.melsom@tarvalley.com>
2018-04-13 22:02:34 +0200

    added keystonejs

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
 
-app.get('/', (req, res) => res.send('Hello, World!'))
+// Require keystone
+var keystone = require('keystone');
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+// Initialise Keystone with your project's configuration.
+// See http://keystonejs.com/guide/config for available options
+// and documentation.
+
+keystone.init({
+	'name': 'Studentföreningen Campus Skellefteå',
+	'brand': 'Studentföreningen Campus Skellefteå',
+
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'pug',
+
+	'emails': 'templates/emails',
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+});
+
+// Load your project's Models
+keystone.import('models');
+
+// Setup common locals for your templates. The following are required for the
+// bundled templates and layouts. Any runtime locals (that should be set uniquely
+// for each request) should be added to ./routes/middleware.js
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+
+// Load your project's Routes
+keystone.set('routes', require('./routes'));
+
+
+// Configure the navigation bar in Keystone's Admin UI
+keystone.set('nav', {
+	posts: ['posts', 'post-categories'],
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	users: 'users',
+});
+
+// Start Keystone to connect to your database and initialise the web server
+
+
+if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+	console.log('----------------------------------------'
+	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
+	+ '\n----------------------------------------'
+	+ '\nYou have opted into email sending but have not provided'
+	+ '\nmailgun credentials. Attempts to send will fail.'
+	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
+	+ '\nset up your mailgun integration');
+}
+
+
+keystone.start();
